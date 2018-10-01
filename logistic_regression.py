9dfba7c4c8060dbaca82dc3ec6db7fa1cf7b79f0
Mc Fair <mcfair18@gmail.com>
2018-10-01 16:10:41 -0700

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,18 +3,18 @@ import numpy as np
 def sigmoid(scores):
     return 1 / (1 + np.exp(-scores))
     
- def log_likelihood(features, target, weights):
+def log_likelihood(features, target, weights):
     scores = np.dot(features, weights)
     ll = np.sum( target*scores - np.log(1 + np.exp(scores)) )
     return ll
     
-  def logistic_regression(features, target, num_steps, learning_rate, add_intercept = False):
+def logistic_regression(features, target, num_steps, learning_rate, add_intercept = False):
     if add_intercept:
         intercept = np.ones((features.shape[0], 1))
         features = np.hstack((intercept, features))
-        
+
     weights = np.zeros(features.shape[1])
-    
+
     for step in xrange(num_steps):
         scores = np.dot(features, weights)
         predictions = sigmoid(scores)
@@ -23,9 +23,9 @@ def sigmoid(scores):
         output_error_signal = target - predictions
         gradient = np.dot(features.T, output_error_signal)
         weights += learning_rate * gradient
-        
+
         # Print log-likelihood every so often
         if step % 10000 == 0:
             print log_likelihood(features, target, weights)
-        
+
     return weights
