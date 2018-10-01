a2262ac0e3a8a4bb747e76a0f7f7cf6b59096df7
Mc Fair <mcfair18@gmail.com>
2018-10-01 16:16:38 -0700

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,31 +1,54 @@
+"""
+https://medium.com/@martinpella/logistic-regression-from-scratch-in-python-124c5636b8ac
+"""
+
 import numpy as np
 
-def sigmoid(scores):
-    return 1 / (1 + np.exp(-scores))
+class LogisticRegression(object):
+    def __init__(self, lr=0.01, num_iter=100000, fit_intercept=True, verbose=False):
+        self.lr = lr
+        self.num_iter = num_iter
+        self.fit_intercept = fit_intercept
     
-def log_likelihood(features, target, weights):
-    scores = np.dot(features, weights)
-    ll = np.sum( target*scores - np.log(1 + np.exp(scores)) )
-    return ll
+    def __add_intercept(self, X):
+        intercept = np.ones((X.shape[0], 1))
+        return np.concatenate((intercept, X), axis=1)
     
-def logistic_regression(features, target, num_steps, learning_rate, add_intercept = False):
-    if add_intercept:
-        intercept = np.ones((features.shape[0], 1))
-        features = np.hstack((intercept, features))
+    def __sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    def __loss(self, h, y):
+        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+    
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+        
+        # weights initialization
+        self.theta = np.zeros(X.shape[1])
+        
+        for i in range(self.num_iter):
+            z = np.dot(X, self.theta)
+            h = self.__sigmoid(z)
+            gradient = np.dot(X.T, (h - y)) / y.size
+            self.theta -= self.lr * gradient
+            
+            if(self.verbose == True and i % 10000 == 0):
+                z = np.dot(X, self.theta)
+                h = self.__sigmoid(z)
+                print(f'loss: {self.__loss(h, y)} \t')
+    
+    def predict_prob(self, X):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+    
+        return self.__sigmoid(np.dot(X, self.theta))
+    
+    def predict(self, X, threshold):
+        return self.predict_prob(X) >= threshold
 
-    weights = np.zeros(features.shape[1])
-
-    for step in xrange(num_steps):
-        scores = np.dot(features, weights)
-        predictions = sigmoid(scores)
-
-        # Update weights with gradient
-        output_error_signal = target - predictions
-        gradient = np.dot(features.T, output_error_signal)
-        weights += learning_rate * gradient
-
-        # Print log-likelihood every so often
-        if step % 10000 == 0:
-            print log_likelihood(features, target, weights)
-
-    return weights
+ if __name__ == '__main__':
+    model = LogisticRegression(lr=0.1, num_iter=300000)
+    pred_y = model.predict(X)
+    # accuracy
+    print np.mean(pred_y == y)
+        
